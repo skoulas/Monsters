387807d9522de16213c08d994c0f49d443e4e43b
skoulas <fotis_skoulas@hotmail.com>
2021-09-03 17:46:16 +0300

    added card-list component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        { name: "Foufoutos", id: "aser1" },
+        { name: "Drakoula", id: "aser2" },
+        { name: "Zombie", id: "aser3" },
+      ],
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
+  }
 }
-
 export default App;
